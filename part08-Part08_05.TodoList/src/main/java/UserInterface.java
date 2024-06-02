f9ae3934620847e1393543b39e2290e86eac3a97
justMartin <martinbarfuss@outlook.de>
2024-06-02 17:08:06 +0200

    100% in part 08_05_TodoList

---
+++ part08-Part08_05.TodoList/src/main/java/UserInterface.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private Scanner scanner;
+    private TodoList todoList;
+    
+    public UserInterface(TodoList todoList, Scanner scanner){
+        this.scanner = scanner;
+        this.todoList = todoList;
+    }
+
+    public void start(){
+        while (true) {
+            System.out.print("Command: "); 
+            String command = scanner.nextLine();
+            if(command.equals("stop")){
+                break;
+            }
+            if(command.equals("add")){
+                System.out.print("To add: ");
+                String task = scanner.nextLine();
+                todoList.add(task);
+            }
+            if(command.equals("list")){
+                todoList.print();
+            }
+            if(command.equals("remove")){
+                System.out.print("Which one is removed? ");
+                int index = Integer.parseInt(scanner.nextLine());
+                todoList.remove(index);
+            }
+
+        }
+    }
+
+}
