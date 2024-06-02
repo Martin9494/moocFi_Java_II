1a62c58fdf5b253170984df1454268414384b434
justMartin <martinbarfuss@outlook.de>
2024-06-02 13:20:57 +0200

    Complete part08-part08_03.LiquidContainers

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,17 +5,32 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Container container1 = new Container("First");
+        Container container2 = new Container("Second");
 
 
         while (true) {
-            System.out.print("> ");
+            System.out.println(container1);
+            System.out.println(container2);
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }else if(input.contains("add")){
+                int number = Integer.parseInt(input.split(" ")[1]);
+                container1.add(number);
+            }else if(input.split(" ")[0].equals("move")){
+                int number = Integer.parseInt(input.split(" ")[1]);
+                container1.move(number, container2);
+            }else if(input.split(" ")[0].equals("remove")){
+                int number = Integer.parseInt(input.split(" ")[1]);
+                container2.remove(number);
+            }else{
+
             }
 
         }
     }
+
 
 }
