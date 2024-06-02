862095c24a0d4fe71ddea26d9a04b5aed4ccd750
justMartin <martinbarfuss@outlook.de>
2024-06-02 16:03:14 +0200

    part 08_04

---
+++ part08-Part08_04.LiquidContainers2/src/main/Container.java
@@ -0,0 +1,58 @@
+
+public class Container {
+    private String name;
+    private int liquit;
+    private int limit;
+
+    public Container(String name){
+        this.name = name;
+        this.liquit = 0;
+        this.limit = 100;
+    }
+
+    public void add(int amount){
+        if(amount < 0){
+
+        }else if (this.liquit + amount >= this.limit) {
+            this.liquit = this.limit;
+        }else if(this.liquit + amount < this.limit){
+            this.liquit += amount;
+        }else{
+
+        }
+    }
+
+    public void move(int amount, Container container){
+        if(amount < 0){
+
+        }else if (amount+container.liquit >= container.limit) {
+            this.liquit = 0;
+            container.liquit = 100;
+        }else if(amount <= this.liquit){
+            container.liquit += amount;
+            this.liquit -= amount;
+        }else if(this.liquit < amount){
+            container.liquit += this.liquit;
+            this.liquit = 0;
+        }else{
+
+        }
+    }
+    public void remove(int amount){
+        if(amount < 0){
+
+        }else if (amount > this.limit) {
+            
+        }else if(this.liquit >= amount){
+            this.liquit = this.liquit - amount;
+        }else if(this.liquit < amount){
+            this.liquit = 0;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.name+": "+this.liquit+"/"+this.limit;
+    }
+
+}
--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -1,21 +1,38 @@
 
+
 import java.util.Scanner;
+import java.awt.Container;
 
 public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container container1 = new Container("First");
+        Container container2 = new Container("Second");
+        
 
         while (true) {
-            System.out.print("> ");
+            System.out.println(container1);
+            System.out.println(container2);
 
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
+            }else if(input.contains("add")){
+                int number = Integer.parseInt(input.split(" ")[1]);
+                container1.add(number);
+            }else if(input.split(" ")[0].equals("move")){
+                int number = Integer.parseInt(input.split(" ")[1]);
+                container1.move(number, container2);
+            }else if(input.split(" ")[0].equals("remove")){
+                int number = Integer.parseInt(input.split(" ")[1]);
+                container2.remove(number);
+            }else{
+
             }
 
         }
     }
+
 
 }
