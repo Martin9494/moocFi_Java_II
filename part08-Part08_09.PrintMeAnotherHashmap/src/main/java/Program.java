357c8df3bd94900e7a9867d180def499c5acf5d7
justMartin <martinbarfuss@outlook.de>
2024-06-03 17:11:28 +0200

    100% on part08_09_printanotherhashmap

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -5,6 +5,25 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your program here!
+        HashMap<String, Book> hashmap = new HashMap<>();
+        hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+        hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "...."));
+
+        printValues(hashmap);
+        System.out.println("---");
+        printValueIfNameContains(hashmap, "prejud");
+    }
+    public static void printValues(HashMap<String,Book> hashmap){
+        for (String key : hashmap.keySet()) {
+            System.out.println(hashmap.get(key));
+        }
+    }
+    public static void printValueIfNameContains(HashMap<String,Book> hashmap, String text){
+        for(String key : hashmap.keySet()){
+            if(hashmap.get(key).getName().contains(text)){
+                System.out.println(hashmap.get(key));
+            }
+        }
     }
 
 }
