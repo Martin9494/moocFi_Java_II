7fc45d6199ccce0daf6717c04fb4fdf34513a92b
justMartin <martinbarfuss@outlook.de>
2024-06-03 12:33:13 +0200

    100% hashmap

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,12 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String,String> object = new HashMap<>();
+        object.put("matthew", "matt");
+        object.put("michael", "mix");
+        object.put("arthur", "artie");
+
+        System.out.println(object.get("matthew"));
     }
 
 }
