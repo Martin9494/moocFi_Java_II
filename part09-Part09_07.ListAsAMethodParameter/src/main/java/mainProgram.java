b197226d403c717eca6e155c39337397fabac6ea
justMartin <martinbarfuss@outlook.de>
2024-06-07 16:18:05 +0200

    100% 09_07 return size of list

--- part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
+++ part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
@@ -1,13 +1,28 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class mainProgram {
 
     public static void main(String[] args) {
         // test your method here
+            List<String> names = new ArrayList<>();
+            names.add("First");
+            names.add("Second");
+            names.add("Third");
+
+            System.out.println(returnSize(names));
 
     }
 
     // Implement here a method returnSize
     // which returns the size of the list given to it
     //as a parameter
+
+    private static int returnSize(List<String> names) {
+        int counter = 0;
+        for (String string : names) {
+            counter++;
+        }
+        return counter;
+    }
 }
