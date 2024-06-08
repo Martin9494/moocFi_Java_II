9a507329cb433def4c209f1f336ee002f098f802
justMartin <martinbarfuss@outlook.de>
2024-06-08 20:23:31 +0200

    100% 10_05 print each line use forEach on list object

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -7,5 +7,17 @@ public class PrintingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> lines = new ArrayList<>();
+
+        while (true) { 
+            String line = scanner.nextLine();
+            if(line.isEmpty()){
+                break;
+            }
+            lines.add(line);
+        }
+
+        lines.forEach(l -> System.out.println(l));
+
     }
 }
