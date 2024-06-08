bb62bb9a48b66116cb09c09247237ff9310020f3
justMartin <martinbarfuss@outlook.de>
2024-06-08 16:00:58 +0200

    100% 10_01 add method get average with stream

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -7,6 +7,28 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Write your program here
+        ArrayList<String> numbers = new ArrayList<>();
+
+        System.out.println("Input numbers, type \"end\" to stop.");
+
+
+        while (true) {
+            String newline = scanner.nextLine();
+            if(newline.equals("end")){
+                break;
+            }
+            numbers.add(newline);
+        }
+
+        double average = numbers.stream().mapToDouble(s -> Integer.parseInt(s))
+        .average().getAsDouble();
+
+        System.out.println("Average number: "+ average);
+
+
 
     }
+
+    //average of the numbers
+    
 }
