cb49d9bc97859043c4390000f4a8000f5edcf686
justMartin <martinbarfuss@outlook.de>
2024-06-08 16:20:43 +0200

    100% part10_02 implement method calculate average for positive or negative values

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,42 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        
+        ArrayList<String> numberList = new ArrayList<>();
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) { 
+            String row = scanner.nextLine();
+            if(row.equals("end")){
+                break;
+            }else if(row.isEmpty()){
+
+            }else{
+                numberList.add(row);
+            }
+            
+        }
+
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String NorP = scanner.nextLine();
+        //average of negative
+        //average of positive
+        double avg = 0.0;
+        if(NorP.equals("n")){
+            avg = numberList.stream()
+            .mapToDouble(v -> Double.valueOf(v))
+            .filter(number -> number < 0)
+            .average().getAsDouble();
+        }else if(NorP.equals("p")){
+            avg = numberList.stream()
+            .mapToDouble(v -> Double.valueOf(v))
+            .filter(number -> number > 0)
+            .average().getAsDouble();
+        }else{
+
+        }
+
+        System.out.println("Average of the numbers: "+avg);
+
 
     }
 }
