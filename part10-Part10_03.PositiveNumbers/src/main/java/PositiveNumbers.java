3c00af7cf4554c3b4679dedf9e55aa17e8aac738
justMartin <martinbarfuss@outlook.de>
2024-06-08 16:40:02 +0200

    100% 10_03 return posive numbers of List with steam map collect(Collectors.toCollection(ArrayList::new)) and (collectors.toList()

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -7,8 +8,32 @@ public class PositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<>();
         // test your method here
 
+        while (true) {
+            String line = scanner.nextLine();
+            
+            if(line.equals("end")){
+                break;
+            }else if(line.equals("")){
+
+            }else{
+                numbers.add(Integer.parseInt(line));
+            }
+            
+        }
+
+        System.out.println(positive(numbers));
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers){
+
+        //numbers= numbers.stream().filter(n -> n > 0).map(n -> n).collect(Collectors.toCollection(ArrayList::new ));
+        numbers= numbers.stream().filter(n -> n > 0).map(n -> n).collect(Collectors.toList());
+
+        return numbers;
     }
 
 }
