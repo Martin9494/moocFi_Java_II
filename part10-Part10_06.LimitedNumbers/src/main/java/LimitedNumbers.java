111a43d42b7da99d75eba9de58115cd4e87dbf90
justMartin <martinbarfuss@outlook.de>
2024-06-09 12:53:27 +0200

    100% 10_06 read input  catch error  use stream to print numbers between 1 and 5 (include)

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,24 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
+        while (true) {
+            
+            try {
+                int number = Integer.parseInt(scanner.nextLine());
+                if(number < 0){
+                    break;
+                }
+                numbers.add(number);
+                
+            } catch (Exception e) {
+                System.out.println("Error: Did you put a number inside? \nErrormessage: "+e.getMessage());
+            }
+            
+        }
+
+        numbers.stream().mapToInt(n -> n).filter(number ->  number >= 1 && number <= 5).forEach(n -> System.out.println(n));
+        
     }
 }
