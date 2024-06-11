fd294b62241e6392d2f83a6792d5f576054f0808
justMartin <martinbarfuss@outlook.de>
2024-06-11 20:07:08 +0200

    100% do regex with vowels, isDayOfWeek(e.g mon|tue|...|sun), isTimeOfDay(e.g 00:00:00 - 23:59:59

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,44 @@
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Checker {
+
+    public boolean isDayOfWeek(String string){
+        //tomuch..
+        //String patternString = "(mon(day)?|tue(s(day)?)?|wed(nes(day)?)?|thu(r(s(day)?)?)?|fri(day)?|sat(ur(day)?)?|sun(day)?)";
+  /*      String patternString = "(mon|tue|wed|thu|fri|sat|sun)";
+        // Create a Pattern object
+        Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+        
+        // Create a Matcher object
+        Matcher matcher = pattern.matcher(string.trim());
+        
+        // Return whether the input matches the pattern
+        return matcher.matches();*/
+
+
+        return string.matches("(mon|tue|wed|thu|fri|sat|sun)");
+    }
+
+    public boolean allVowels(String string){
+        String patternString = "^[aeiouAEIOU]+$";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(string.trim());
+        return matcher.matches();
+    
+    }
+    public boolean timeOfDay(String string){
+                // Define a pattern to match valid HH:MM:SS time (correct regex)
+                String patternString = "([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]";
+        
+                // Create a Pattern object
+                Pattern pattern = Pattern.compile(patternString);
+                
+                // Create a Matcher object
+                Matcher matcher = pattern.matcher(string.trim());
+                
+                // Return whether the input matches the pattern
+                return matcher.matches();
+    }
 
 }
--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,21 @@
 
 
+
+
+
+
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Checker check = new Checker();
+        String[] testInputs = {"04:59:31", "12:34:56", "23:59:59", "24:00:00", "00:60:00", "00:00:60", "12:345:56"};
+
+        // Print results of matching each test input
+        for (String input : testInputs) {
+            System.out.println(input + ": "+check.timeOfDay(input));
+            
+        }
     }
 }
