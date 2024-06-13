e04f580634bf4e502b967981c6619dbe98567e09
justMartin <martinbarfuss@outlook.de>
2024-06-13 09:12:32 +0200

    add Place Class in domain

--- part11-Part11_10.FlightControl/src/main/java/FlightControl/domain/Place.java
+++ part11-Part11_10.FlightControl/src/main/java/FlightControl/domain/Place.java
@@ -1,5 +1,14 @@
 package FlightControl.domain;
 
 public class Place {
+    private String place;
+
+    public Place (String place){
+        this.place = place;
+    }
+
+    public String getPlace() {
+        return place;
+    }
 
 }
