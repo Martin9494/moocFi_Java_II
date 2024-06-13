9bd704b69e1ede259bdc899e1ce92e065b3c24ef
justMartin <martinbarfuss@outlook.de>
2024-06-13 09:10:59 +0200

    add Airplane Class in domain

--- part11-Part11_10.FlightControl/src/main/java/FlightControl/domain/Airplane.java
+++ part11-Part11_10.FlightControl/src/main/java/FlightControl/domain/Airplane.java
@@ -2,4 +2,23 @@ package FlightControl.domain;
 
 public class Airplane {
 
+    private String ID;
+    private int capacity;
+
+    public Airplane(String ID, int capacity){
+        this.ID = ID;
+        this.capacity = capacity;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+    
+
+
+
 }
